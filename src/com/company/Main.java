97a28056a9b7ec162b1e905ff51c6f74080cc59a
Zzeyrol <alex_good2510@mail.ru>
2021-11-03 17:42:56 +0300

    cylendervolume

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double radius = 4;
+        double height = 6;
+        double volume = 3.1416 * radius * radius * height;
+
+        System.out.println("Объем цилиндра равен " + volume);
+    }
+}
